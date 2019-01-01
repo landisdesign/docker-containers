d405bc964530bfa084f6f3d3fe8d620ed085fe2f
Michael Landis <michael4css@gmail.com>
2018-12-31 19:15:11 -0800

    Updated java-alpine to auto-expand modules instead of requiring manual labor

--- java-alpine/README.md
+++
@@ -1,5 +0,0 @@
-BEFORE USING THIS CONTAINER:
-
-The file jdk-12/lib/modules was too big for Git. It has been compressed to modules.zip.
-
-Unzip module.zip before building.
--- java-alpine/paxctl.sh
+++ java-alpine/paxctl.sh
@@ -1,9 +1,15 @@
 if ! which paxctl; then
 	apk add paxctl
+	paxctl_added=1
 fi
 
 paxctl -c java
 paxctl -m java
+
+if [ "$paxctl_added" ]
+then
+	apk del paxctl
+fi
 
 if which setfattr; then
 	setfattr -n user.pax.flags -v "mr" java
